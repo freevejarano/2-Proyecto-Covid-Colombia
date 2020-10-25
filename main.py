6c8dce1fe50c00b044152ef5260e15c6affc2236
alejandro <luis.vejarano01@correo.usa.edu.co>
2020-10-24 21:36:58 -0500

    Agrego Gráfica de Torta de Casos Por Localidad en Bogotá

--- main.py
+++ main.py
@@ -14,18 +14,47 @@ from datetime import date
 #Se obtiene la fecha actual para el nombre del PNG generado por cada gráfica
 hoy=time.strftime("%d-%m-%y")
 
-
+#URL API Datos Abiertos de Bogotá
 urlDatos = 'https://datosabiertos.bogota.gov.co/api/3/action/datastore_search_sql?'
-#urlDatosSQL = 'sql=SELECT "LOCALIDAD_ASIS", count(*) as Cantidad from "b64ba3c4-9e41-41b8-b3fd-2da21d627558" group by "LOCALIDAD_ASIS" order by "LOCALIDAD_ASIS"'
-urlDatosSQL = 'sql=SELECT "FECHA_DIAGNOSTICO", count(*) as cantidad from "b64ba3c4-9e41-41b8-b3fd-2da21d627558" group by "FECHA_DIAGNOSTICO" order by "FECHA_DIAGNOSTICO"'
 
-req = requests.get(url=urlDatos+urlDatosSQL)
-reqJson = req.json() #Organizar datos
-#aux=reqJson['result']['records']
-#print(reqJson)
-ndict=reqJson['result']['records']
-lis=[]
-for x in ndict:
+#Consulta SQL a la API
+urlDatosSQL1 = 'sql=SELECT "LOCALIDAD_ASIS" as localidad, count(*) as cantidad from "b64ba3c4-9e41-41b8-b3fd-2da21d627558" group by "LOCALIDAD_ASIS" order by "LOCALIDAD_ASIS"'
+urlDatosSQL2 = 'sql=SELECT "FECHA_DIAGNOSTICO", count(*) as cantidad from "b64ba3c4-9e41-41b8-b3fd-2da21d627558" group by "FECHA_DIAGNOSTICO" order by "FECHA_DIAGNOSTICO"'
+
+#Peitición de datos, conversión de json a lista de diccionarios
+req1 = requests.get(url=urlDatos+urlDatosSQL1)
+reqJson1 = req1.json() #Organizar datos
+ndict1=reqJson1['result']['records']
+
+#Organización de localidades, correción de los casos "sin dato"
+localidad=[]
+cantloca=[]
+for x in ndict1:
+    if x['localidad']!=None:
+        localidad.append(x['localidad'])
+        cantloca.append(int(x['cantidad']))
+    else:
+        aux=localidad.index('Sin dato')
+        cantloca[aux]+=int(x['cantidad'])
+
+#Gráfico de Torta Casos Por Localidad
+fig1, ax1 = plt.subplots(figsize=(20,20))
+plt.title("CASOS CONFIRMADOS POR LOCALIDAD DE COVID-19 EN BOGOTA\n", fontdict={'fontsize':15})
+ax1.pie(cantloca, labels=localidad, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')
+#fname="GraficoTorta_Localidad_Covid_Bogota_"+hoy+".png"
+#plt.savefig(fname, bbox_inches='tight')
+
+
+#Peitición de datos, conversión de json a lista de diccionarios
+req2 = requests.get(url=urlDatos+urlDatosSQL2)
+reqJson2 = req2.json() #Organizar datos
+ndict2=reqJson2['result']['records']
+
+#Correción formato de fecha
+lis2=[]
+for x in ndict2:
     fecha = x['FECHA_DIAGNOSTICO']
     if(isinstance(fecha,str)):
      d=datetime.datetime.strptime(fecha, "%d/%m/%Y").strftime("%Y-%m-%d")
@@ -33,12 +62,13 @@ for x in ndict:
      dit={}
      dit['fecha']=e
      dit['cantidad']=int(x['cantidad'])
-     lis.append(dit)
+     lis2.append(dit)
 
+#Organización casos por día en casos por mes
 cant=[0,0,0,0,0,0,0,0]
 fech=[]
 meses=['Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre']
-for x in lis:
+for x in lis2:
     if (x['fecha'].month==3):
         cant[0]+=x['cantidad']
     elif (x['fecha'].month==4):
@@ -56,35 +86,19 @@ for x in lis:
     elif (x['fecha'].month==10):
         cant[7]+=x['cantidad']
 
-h=0
-for x in range(len(cant)):
-    print(meses[x]," ",cant[x])
-    h+=cant[x]
-
-print(h)
-
+#Gráfica de Barras Casos Por Mes
 fig, ax = plt.subplots(figsize=(9, 7))
 ax.set_ylabel('NÚMERO DE CASOS')
 ax.set_title('CASOS CONFIRMADOS DE COVID-19 EN BOGOTÁ POR MESES')
 plt.bar(meses, cant)
-#fname="GraficoBarras_Edad_Covid_Colombia_"+hoy+".png"
+#fname="GraficoBarras_Mes_Covid_Bogota_"+hoy+".png"
 #plt.savefig(fname, bbox_inches='tight')
 plt.tight_layout()
 
+#Muestra todos los gráficos
 plt.show()
 
 
-
-
-#t = date.fromisoformat('2020-12-04')
-#f= date.fromisoformat('2020-12-05')
-#print(f.year)
-
-
-
-
-#df_bogota = gpd.read_file('data//bogota_cadastral.json')
-#df_bogota.MOVEMENT_ID = df_bogota.MOVEMENT_ID.astype(np.int64)
 
 
 
